9190b4f86c0c583d7def872da4cb431cf3bd521e
Michael Huber <huber@medien-haus.de>
2022-05-31 14:44:29 +0200

    Init

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'main',
+    loadChildren: () => import('./mainpage/mainpage.module').then(m => m.MainpageModule)
+  },
+  {
+    path: '',
+    redirectTo: 'main',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
